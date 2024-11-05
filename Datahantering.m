aebb7736ae0e767d31dc930098f8a6e60dbbeafe
VilgotSamuelsson <vilgotaik@gmail.com>
2024-11-05 11:46:25 +0100

    datauppdatering

--- Datahantering.m
+++ Datahantering.m
@@ -1,1 +1,3 @@
-[A,R] = readgeoraster("C:\Users\vilgo\OneDrive\Desktop\Projekt WindBaseload\c76a760b03314b5fce3c90e342a95e93.grib");
+f = 28;
+
+[A,R] = readgeoraster("C:\Users\vilgo\OneDrive\Desktop\Projekt WindBaseload\Windpower-base-load-model\c76a760b03314b5fce3c90e342a95e93.grib");
