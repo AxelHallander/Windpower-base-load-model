de0d48bab9bbc27586efc5c2580aab2cf260477e
AxelHallander <axel.hallander.7781@student.uu.se>
2024-11-07 16:03:23 +0100

    - created testfile for multiple parks - added a sum value to power_calculations

--- ReadWindData.m
+++ ReadWindData.m
@@ -12,6 +12,6 @@ for i = 1:length(A)/2
     east_comp = A(:,:,2*i-1);
     north_comp = A(:,:,2*i);
 
-    Wind_speed_momentarily = real(sqrt(east_comp.^2 + north_comp^2));
+    Wind_speed_momentarily = real(sqrt(east_comp.^2 + north_comp.^2));
     wind_speed(:,:,i) = Wind_speed_momentarily;
 end
